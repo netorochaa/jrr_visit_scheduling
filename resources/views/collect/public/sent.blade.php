d2156a53fecbfdf9b125adbff7ba4a006b452298
netorochaa <jrr.neto@hotmail.com>
2020-05-18 23:29:45 -0300

    Update sent.blade.php

--- resources/views/collect/public/sent.blade.php
+++ resources/views/collect/public/sent.blade.php
@@ -13,12 +13,12 @@
                             <h1 style="color: red">{{ session('return')['message'] }}</h1>
                         @else
                             <h1 style="color: lightgreen">{{ session('return')['message'] }}</h1>
-                            <p>{{ session('return')['describe'] }}</p>
+                            <p>{{ session('return')['describe'] }}</p><br>
                             <p>{{ session('return')['text'] }}</p>
                         @endif
-                        <h2>Em breve o laboratório entrará em contato pelo telefone ou e-mail informado para CONFIRMAR seu agendamento.</h2>
+                        <h2>Em breve o laboratório entrará em contato pelo telefone ou e-mail para CONFIRMAR seu agendamento.</h2>
                         <h3 class="lead">
-                            Prezado cliente, este agendamento será avaliado de acordo com a disponibilidade e estará sujeito a alterações na data ou horário.<br>
+                            Prezado cliente, este agendamento será avaliado de acordo com a disponibilidade e estará sujeito a alterações na data ou horário.<br><br>
                             O AGENDAMENTO PARA DETECÇÃO DO COVID-19 NÃO ESTA SENDO REALIZADO POR MEIO DESTE FORMULÁRIO, É NECESSÁRIO QUE O CLIENTE ENTRE EM CONTATO COM O
                             LABORATÓRIO PELA CENTRAL TELEFÔNICA OU WHATSAPP.</h3>
                     @endif
